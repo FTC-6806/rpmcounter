9fddb2d9054cc9573338f08dade9c95ca99cc74a
Liam Marshall <archimedespi3141@gmail.com>
2014-11-18 22:41:33 -0600

    Switching between modes.

--- RPMCounterFreqCount/RPMCounterFreqCount.ino
+++ RPMCounterFreqCount/RPMCounterFreqCount.ino
@@ -1,29 +1,57 @@
-/* FreqCount - Example with LCD output
- * http://www.pjrc.com/teensy/td_libs_FreqCount.html
- *
- * This example code is in the public domain.
- */
 #include <FreqCount.h>
 #include <Wire.h>
 #include <Adafruit_MCP23017.h>
 #include <Adafruit_RGBLCDShield.h>
 
-Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
+#define HERTZ_TO_RPM(x) x * 60
 
+enum Mode {
+  frequency,
+  rpm
+} mode;
+
+Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
 
 void setup() {
   Serial.begin(57600);
   lcd.begin(16, 2);
-  lcd.print("Freq:");
+  mode = frequency;
+  updateMode();
   FreqCount.begin(1000);
 }
 
 void loop() {
   if (FreqCount.available()) {
-    unsigned long count = FreqCount.read();
+    unsigned long hertz = FreqCount.read();
     lcd.setCursor(0, 1);
-    lcd.print(count);
-    lcd.print(" Hz      ");
+    if (mode == frequency) {
+      lcd.print(hertz);
+      lcd.print(" Hz      ");
+    } else if (mode == rpm) {
+      lcd.print(HERTZ_TO_RPM(hertz));
+      lcd.print(" RPM     ");
+    }
+  }
+  
+  uint8_t buttons = lcd.readButtons();
+  if (buttons) {
+    if (buttons & BUTTON_UP){
+      mode = frequency;
+      updateMode();
+    }
+    if (buttons & BUTTON_DOWN) {
+      mode = rpm;
+      updateMode();
+    }
   }
 }
 
+void updateMode() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  if (mode == frequency) {
+    lcd.print("Frequency:");
+  } else if (mode == rpm) {
+    lcd.print("RPM:");
+  }
+}
